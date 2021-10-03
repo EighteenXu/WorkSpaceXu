0b430b09784aa2668dc5b2b6f9a633cbce60237f
Eight <1078274540@qq.com>
2021-10-03 16:09:21 +0800

    week2

---
+++ week2/OkHttpUtils.java
@@ -0,0 +1,32 @@
+package com.example.demo2;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class OkHttpUtils {
+	// 缓存客戶端实例
+	public static OkHttpClient client = new OkHttpClient();
+
+	// get调用
+	public static String getAsString(String url) throws IOException {
+		Request request = new Request.Builder().url(url).build();
+
+		try (Response response = client.newCall(request).execute()) {
+			return response.body().string();
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		//String url = "https://square.github.io/okhttp";
+    	String url = "http://localhost:8802";
+		String text = OkHttpUtils.getAsString(url);
+		System.out.println("url:" + url + ";response" + text);
+		// 清理资源
+		OkHttpUtils.client = null;
+	}
+
+}
