8f1ed837fd41a3df0657c21d5c42744973a0d4b3
Eight <1078274540@qq.com>
2021-10-04 00:45:29 +0800

    week3

---
+++ week3/nio02/src/main/java/io/github/kimmking/gateway/filter/ProxyBizFilter.java
@@ -0,0 +1,36 @@
+package io.github.kimmking.gateway.filter;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultHttpHeaders;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaders;
+
+
+/**
+ * 过滤器
+ * @author zhuxuli
+ *
+ */
+public class ProxyBizFilter implements HttpRequestFilter{
+
+	public static ProxyBizFilter newInstance(){
+		return new ProxyBizFilter();
+	}
+	
+	@Override
+	public void filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+		String uri = fullRequest.uri();
+        System.out.println(" filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx)接收到的请求,url: " + uri);
+		if (uri.startsWith("/hello")) {
+			
+		}else {
+			throw new RuntimeException("不支持的uri"+uri);
+		}
+		HttpHeaders headers =fullRequest.headers();
+		if (headers==null) {
+			headers=new DefaultHttpHeaders();
+		}
+		headers.add("proxy-tag",this.getClass().getSimpleName());
+	}
+
+}
